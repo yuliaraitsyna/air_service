82c1cabd7fa802149d222d5621d350419eb5df93
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-03-19 21:32:20 +0300

    feat: create db connection

---
+++ booking_service/app/core/database.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
