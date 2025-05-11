25a0fbb1d441762872d207ce3fdc7d754fa2c2bb
yuliaraitsyna <yuliaraitsyna@gmail.com>
2025-05-11 16:33:59 +0300

    feat: add cors origin

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import auth
 from app.api.routes import booking
 
 app = FastAPI(title="Flight Booking Microservice")
+
+origins = [
+    os.getenv("LOCAL_DEV_URL"),  # React dev server
+    os.getenv("PROD_URL"),  # Production frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(booking.router, prefix="/booking")
